b004866367e99e5dde2f228991edc09895bdc61a
A TR <srogers.design@gmail.com>
2021-09-23 18:27:30 -0600

    Updated style sheets and added landing page routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import {HomeComponent} from './routes/home/home.component';
+// import {ContactComponent} from './routes/contact';
+
+const routes: Routes = [
+{
+  path:'',
+  component: HomeComponent,
+  children: [
+    // {
+    //   path: 'search',
+    //   component: SearchComponent
+    // }
+  ]
+},
+// {
+//   path: 'contact',
+//   loadChildren: '.routes/contact/contact.module#ContactModule'
+// }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HomeComponent } from './routes/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
